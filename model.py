6da5cfb5d535bb24da688987d98c7f320e4885f4
guozihankr-sys <guozihankr@163.com>
2026-04-18 23:32:41 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,23 +1,7 @@
-from PIL import Image
-import numpy as np
-import cv2
+import random
 
 def predict(img_path):
-    # 读取图片
-    image = Image.open(img_path).convert("L")
-    img = np.array(image)
-
-    # 边缘检测
-    edges = cv2.Canny(img, 50, 150)
-    edge_count = np.sum(edges > 0)
-
-    # 亮度
-    brightness = np.mean(img)
-
-    # 简单规则（比随机聪明很多）
-    score = edge_count / 10000 + brightness / 255
-
-    prob = round(min(score, 1.0), 2)
+    prob = round(random.uniform(0, 1), 2)  # 改这里
 
     if prob > 0.5:
         return "检测到骨折", prob
