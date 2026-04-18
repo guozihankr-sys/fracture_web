2c706409ad3ba612ea4a5e48a3d98bc8a6c3886d
guozihankr-sys <guozihankr@163.com>
2026-04-18 21:52:32 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,23 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import torchvision.models as models
 
+model = models.resnet18(pretrained=True)
+model.fc = torch.nn.Linear(model.fc.in_features, 2)
+model.eval()
+
+transform = transforms.Compose([
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+])
+
+def predict(img_path):
+    img = Image.open(img_path).convert('RGB')
+    img_tensor = transform(img).unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(img_tensor)
+        prob = torch.softmax(output, dim=1)
+
+    return prob[0][1].item()
