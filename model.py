d98f97edeea1f56cee956b96c826ab3c0c465069
guozihankr-sys <guozihankr@163.com>
2026-04-18 22:23:26 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,9 @@
 import random
 
 def predict(img_path):
-    return random.uniform(0.3, 0.9)
+    prob = round(random.uniform(0.3, 0.9), 2)
+
+    if prob > 0.5:
+        return "fracture", prob
+    else:
+        return "normal", prob
