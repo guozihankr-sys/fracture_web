f053632fef6c9e2c5bec1c4d053bfb5857b29f55
guozihankr-sys <guozihankr@163.com>
2026-04-18 23:30:47 +0900

    Update model.py

--- model.py
+++ model.py
@@ -1,26 +1,9 @@
-from PIL import Image
-import numpy as np
+import random
 
 def predict(img_path):
-    image = Image.open(img_path).convert("L")
-    img = np.array(image)
+    prob = round(random.uniform(0, 1), 2)  # 改这里
 
-    # 对比度
-    contrast = img.std()
-
-    # 亮度
-    brightness = img.mean()
-
-    # ⭐ 重新设计评分（更合理）
-    contrast_score = contrast / 80      # 控制范围
-    brightness_score = brightness / 255
-
-    # 加权（重点）
-    score = 0.6 * contrast_score + 0.4 * brightness_score
-
-    prob = round(min(score, 1.0), 2)
-
-    if prob > 0.55:
+    if prob > 0.5:
         return "检测到骨折", prob
     else:
         return "未检测到骨折", prob
